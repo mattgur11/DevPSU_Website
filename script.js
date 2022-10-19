8361a8e5f1949ec1fbc3a748bfb6e696c4e6fe5c
mattgur11 <75272970+mattgur11@users.noreply.github.com>
2022-10-18 20:34:30 -0400

    Added more JavaScript and jQuery functionality

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+$(document).ready(function() {
+    $("#btn1").click(function(){
+        //alert("mouseenter");
+        //$("p.paragraph").toggle(); //# shows that it is an id, . is for class, [] for attribute (like [href])
+        //$("#div1").slideToggle(3000);
+        let div = $("div")
+        div.animate({left: "400px", height: "300px", opacity: "0.5"}, "fast")
+    }
+    );
+}
+);
+
+
+
+
+
+
+
+
 document.getElementById('demo').innerHTML = 'new text';
-document.getElementById('demo').id = 'demo2';
-document.getElementById('demo2').style.color = "red";
-document.getElementById('demo2').outerHTML = "<button>a new button</button>"
+//document.getElementById('demo').id = 'demo2';
+document.getElementById('demo').style.color = "red";
+//document.getElementById('demo').outerHTML = "<button>a new button</button>"
 
 function myFun(){
     alert("Alert message");
 }
 
-myFun();
+//myFun();
 
 function darkMode(){
     document.getElementById("pagestyle").setAttribute("href", "./style-dark.css");
@@ -21,3 +40,18 @@ a+=10;
 b+=10;
 
 console.log(a+b+c);
+
+
+function keyPressed(input){
+    console.log(input);
+    document.getElementById("keypressed_text").innerHTML = input;
+}
+
+const key = function keyEvent(e){
+    keyPressed(e.key);
+    if (e.key === "f"){
+        alert("f key was pressed");
+    }
+}
+
+document.addEventListener("keypress", key);
